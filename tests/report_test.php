65fd2ed9773e8d6912c162e47a86e4566c66de9d
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2026-05-01 18:02:09 +0200

    another test fix

--- tests/report_test.php
+++ tests/report_test.php
@@ -232,7 +232,7 @@ final class report_test extends \advanced_testcase {
         $reflectedoptions = $reflectedreport->getProperty('options');
         $reflectedoptions->setAccessible(true);
         $options = new quiz_essaydownload_options('essaydownload', $quiz, $cm, $course);
-        $options->nametemplate = '%firstname% %lastname%';
+        $options->nametemplate = '%lastname% %firstname%';
         $reflectedoptions->setValue($report, $options);
 
         // Fetch the attemps using the report's API.
